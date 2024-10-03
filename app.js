15141a9e5e8719c85ffd81a1a0511fef6b2a757a
fcuadra-99 <fcuadra_230000001491@uic.edu.ph>
2024-10-03 20:07:09 +0800

    added archive

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
 import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
-import { gsap } from 'https://cdn.skypack.dev/gsap';
 
 const camera = new THREE.PerspectiveCamera(
     10,
     window.innerWidth / window.innerHeight,
     0.1,
-    2000
+    500
 );
 camera.position.z = 4;
 camera.position.y = 0.2;
@@ -27,15 +26,14 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById('container3D').appendChild(renderer.domElement);
 
 const reRender3D = () => {
-    scene.rotateY(.1);
     requestAnimationFrame(reRender3D);
+    scene.rotateY(.08);
     renderer.render(scene, camera);
 };
 reRender3D();
 
-
 window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.aspect = (window.innerWidth / window.innerHeight);
     camera.updateProjectionMatrix();
 })

b295ace2e27f7e3f68fb5fabfe63cc3efe9b1504
fcuadra-99 <fcuadra_230000001491@uic.edu.ph>
2024-10-05 14:22:15 +0800

    as

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ controls.maxDistance = 10;
 controls.minDistance = 3;
 controls.enablePan = false;
 
-
 camera.position.set(4, 0, 1);
 
 const scene = new THREE.Scene();
@@ -58,11 +57,42 @@ window.addEventListener("resize", () => {
   camera.updateProjectionMatrix();
 });
 
-window.addEventListener('dblclick', event => {
+window.addEventListener("dblclick", (event) => {
+  reset();
+});
+
+function dbtap() {
+  let last = 0;
+  let tm;
+  return function dbtap(event) {
+    const curTime = new Date().getTime();
+    const tapLen = curTime - last;
+    if (tapLen < 230 && tapLen > 0) {
+      reset();
+    } else {
+      tm = setTimeout(() => {
+        clearTimeout(tm);
+      }, 230);
+    }
+    last = curTime;
+  };
+}
+
+console.log(navigator.userAgent);
+
+if (
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  )
+) {
+  document.body.addEventListener("touchend", dbtap());
+}
+
+function reset() {
   gsap.to(camera.position, {
     duration: 1.5,
     x: 4,
     y: 0,
     z: 1,
   });
-});
+}
